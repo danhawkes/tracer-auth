0d806e07c511369a7bb88da3ce9b10b9a3949797
Daniel Hawkes <dan@danhawkes.co.uk>
2015-04-04 21:48:31 +0100

    more

--- gulpconfig.js
+++ gulpconfig.js
@@ -1,7 +1,7 @@
 module.exports = [
   {pattern: 'CONFIG_DB_PROTOCOL', replacement: process.env.DB_PROTOCOL || 'http'},
-  {pattern: 'CONFIG_DB_PORT', replacement: process.env.PORT || process.env.DB_PORT || '5984'},
-  {pattern: 'CONFIG_DB_HOST', replacement: process.env.DB_HOST || 'localhost'},
-  {pattern: 'CONFIG_WEB_PORT', replacement: process.env.PORT || process.env.WEB_PORT || '8080'},
+  {pattern: 'CONFIG_DB_PORT', replacement: process.env.DB_PORT || '80'},
+  {pattern: 'CONFIG_DB_HOST', replacement: process.env.DB_HOST || 'tracer-db.arcs.co'},
+  {pattern: 'CONFIG_WEB_PORT', replacement: process.env.WEB_PORT || '80'},
   {pattern: 'CONFIG_DB_ADMIN_PASS', replacement: process.env.DB_ADMIN_PASS || 'admin'}
 ];
